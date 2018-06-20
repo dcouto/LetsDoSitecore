602e2247b5ef4d154d3fc138243377678a0f6ef8
Douglas Couto <douglas.b.couto@gmail.com>
2018-06-20 13:08:21 -0400

    added Coveo IsPublishableInboundFilter

---
+++ src/Foundation/Coveo/code/LetsDoSitecore.Foundation.Coveo/InboundFilters/IsPublishableInboundFilter.cs
@@ -0,0 +1,23 @@
+﻿using Coveo.SearchProvider.InboundFilters;
+using Coveo.SearchProvider.Pipelines;
+using LetsDoSitecore.Foundation.Extensions;
+
+namespace LetsDoSitecore.Foundation.Coveo.InboundFilters
+{
+    public class IsPublishableInboundFilter : AbstractCoveoInboundFilterProcessor
+    {
+        public override void Process(CoveoInboundFilterPipelineArgs p_Args)
+        {
+            if (p_Args.IndexableToIndex != null
+                && p_Args.IndexableToIndex.Item != null
+                && p_Args.IndexableToIndex.Item.SitecoreItem != null
+                && !p_Args.IsExcluded
+                && ShouldExecute(p_Args))
+            {
+                var scItem = p_Args.IndexableToIndex.Item.SitecoreItem;
+
+                p_Args.IsExcluded = !scItem.AllAncestorsAndSelfArePublishable();
+            }
+        }
+    }
+}
