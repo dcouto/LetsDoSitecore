1286f533c43b426a5e8f3a9f64580a957da412ae
Douglas Couto <DCouto@rightpoint.com>
2018-06-20 11:38:04 -0400

    Add project files.

---
+++ src/Foundation/Coveo/code/LetsDoSitecore.Foundation.Extensions/ItemExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Sitecore.Data.Items;
+
+namespace LetsDoSitecore.Foundation.Extensions
+{
+    public static class ItemExtensions
+    {
+        public static bool AllAncestorsAndSelfArePublishable(this Item item)
+        {
+            if (item.Publishing.NeverPublish || (item.Parent != null && item.Parent.Publishing.NeverPublish))
+            {
+                return false;
+            }
+
+            if (item.Parent != null)
+            {
+                return AllAncestorsAndSelfArePublishable(item.Parent);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/Foundation/Coveo/tests/LetsDoSitecore.Foundation.Extensions.Tests/ItemExtensionsTests.cs
@@ -0,0 +1,221 @@
+﻿using Sitecore.FakeDb;
+using Xunit;
+using LetsDoSitecore.Foundation.Extensions;
+
+namespace LetsDoSitecore.Foundation.Extensions.Tests
+{
+    public class ItemExtensionsTests
+    {
+        [Fact]
+        public void When_ItemIsHomeAndIsPublishable_Expect_AllAncestorsAndSelfToBeTrue()
+        {
+            var expected = true;
+
+            using(var db = new Db
+            {
+                new DbItem("Home")
+            })
+            {
+                var home = db.GetItem("/sitecore/content/home");
+
+                var actual = home.AllAncestorsAndSelfArePublishable();
+
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        [Fact]
+        public void When_ItemIsChildOfHomeIsPublishableAndHomeIsPublishable_Expect_AllAncestorsAndSelfToBeTrue()
+        {
+            var expected = true;
+
+            using(var db = new Db
+            {
+                new DbItem("Home")
+                {
+                    new DbItem("Sample Item 1")
+                }
+            })
+            {
+                var sampleItem1 = db.GetItem("/sitecore/content/home/sample item 1");
+
+                var actual = sampleItem1.AllAncestorsAndSelfArePublishable();
+
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        [Fact]
+        public void When_ItemIsDescendantOfHomeIsPublishableAndAllAncestorsArePublishable_Expect_AllAncestorsAndSelfToBeTrue()
+        {
+            var expected = true;
+
+            using(var db = new Db
+            {
+                new DbItem("Home")
+                {
+                    new DbItem("Sample Item 1")
+                    {
+                        new DbItem("Sample Descendant 1")
+                        {
+                            new DbItem("Sample Descendant 2")
+                        }
+                    }
+                }
+            })
+            {
+                var sampleDescendant2 = db.GetItem("/sitecore/content/home/sample item 1/sample descendant 1/sample descendant 2");
+
+                var actual = sampleDescendant2.AllAncestorsAndSelfArePublishable();
+
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        [Fact]
+        public void When_ItemIsHomeAndIsNotPublishable_Expect_AllAncestorsAndSelfToBeFalse()
+        {
+            var expected = false;
+
+            using (var db = new Db
+            {
+                new DbItem("Home")
+                {
+                    { "__Never publish", "1" }
+                }
+            })
+            {
+                var home = db.GetItem("/sitecore/content/home");
+
+                var actual = home.AllAncestorsAndSelfArePublishable();
+
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        [Fact]
+        public void When_ItemIsChildOfHomeIsPublishableButHomeIsNotPublishable_Expect_AllAncestorsAndSelfToBeFalse()
+        {
+            var expected = false;
+
+            using (var db = new Db
+            {
+                new DbItem("Home")
+                {
+                    { "__Never publish", "1" },
+
+                    new DbItem("Sample Item 1")
+                }
+            })
+            {
+                var sampleItem1 = db.GetItem("/sitecore/content/home/sample item 1");
+
+                var actual = sampleItem1.AllAncestorsAndSelfArePublishable();
+
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        [Fact]
+        public void When_ItemIsChildOfHomeIsNotPublishableButHomeIsPublishable_Expect_AllAncestorsAndSelfToBeFalse()
+        {
+            var expected = false;
+
+            using (var db = new Db
+            {
+                new DbItem("Home")
+                {
+                    new DbItem("Sample Item 1")
+                    {
+                        { "__Never publish", "1" },
+                    }
+                }
+            })
+            {
+                var sampleItem1 = db.GetItem("/sitecore/content/home/sample item 1");
+
+                var actual = sampleItem1.AllAncestorsAndSelfArePublishable();
+
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        [Fact]
+        public void When_ItemIsChildOfHomeIsNotPublishableAndHomeIsNotPublishable_Expect_AllAncestorsAndSelfToBeFalse()
+        {
+            var expected = false;
+
+            using (var db = new Db
+            {
+                new DbItem("Home")
+                {
+                    { "__Never publish", "1" },
+
+                    new DbItem("Sample Item 1")
+                    {
+                        { "__Never publish", "1" },
+                    }
+                }
+            })
+            {
+                var sampleItem1 = db.GetItem("/sitecore/content/home/sample item 1");
+
+                var actual = sampleItem1.AllAncestorsAndSelfArePublishable();
+
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        [Fact]
+        public void When_ItemIsDesendantOfHomeIsPublishableButParentIsNotPublishable_Expect_AllAncestorsAndSelfToBeFalse()
+        {
+            var expected = false;
+
+            using (var db = new Db
+            {
+                new DbItem("Home")
+                {
+                    new DbItem("Sample Item 1")
+                    {
+                        { "__Never publish", "1" },
+
+                        new DbItem("Sample Descendant 1")
+                    }
+                }
+            })
+            {
+                var sampleDescendant1 = db.GetItem("/sitecore/content/home/sample item 1/sample descendant 1");
+
+                var actual = sampleDescendant1.AllAncestorsAndSelfArePublishable();
+
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        [Fact]
+        public void When_ItemIsDescendantOfHomeIsPublishableButHomeIsNotPublishable_Expect_AllAncestorsAndSelfToBeFalse()
+        {
+            var expected = false;
+
+            using (var db = new Db
+            {
+                new DbItem("Home")
+                {
+                    { "__Never publish", "1" },
+
+                    new DbItem("Sample Item 1")
+                    {
+                        new DbItem("Sample Descendant 1")
+                    }
+                }
+            })
+            {
+                var sampleDescendant1 = db.GetItem("/sitecore/content/home/sample item 1/sample descendant 1");
+
+                var actual = sampleDescendant1.AllAncestorsAndSelfArePublishable();
+
+                Assert.Equal(expected, actual);
+            }
+        }
+    }
+}
